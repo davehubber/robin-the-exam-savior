27257f4f4629c104d788b93fba782385b0750cd9
David Cordeiro <davcordeiro03@gmail.com>
2025-03-03 10:42:05 +0000

    Implement portal mechanic

---
+++ Assets/Scripts/Interactable Objects/Portal.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    // The point where the player will appear when using this door.
+    public Transform exitPoint;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
