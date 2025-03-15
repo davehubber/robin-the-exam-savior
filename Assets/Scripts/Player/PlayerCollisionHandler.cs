f483318993945d26ea8fa938fc4a762b31196fe2
David Cordeiro <up202108820@g.uporto.pt>
2025-03-15 20:16:01 +0000

    Finish floor 1 configuration in the editor

--- Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -2,11 +2,22 @@ using UnityEngine;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    // Reference to the movement controller (should be on the same GameObject).
+    public MovementController movementController;
     // Reference to the interaction controller (should be on the same GameObject).
     public PlayerInteractionController interactionController;
 
     private void Awake()
     {
+        if (movementController == null)
+        {
+            movementController = GetComponent<MovementController>();
+            if (movementController == null)
+            {
+                Debug.LogError("PlayerCollisionHandler requires a MovementController component.");
+            }
+        }
+
         if (interactionController == null)
         {
             interactionController = GetComponent<PlayerInteractionController>();
@@ -19,8 +30,13 @@ public class PlayerCollisionHandler : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        // Handle trap interaction: activate slowdown when entering a SlowdownTrap.
+        if (other.CompareTag("SlowdownTrap"))
+        {
+            movementController.SetSlowDown(true);
+        }
         // Check using layer or tag; here we assume objects to interact with are on the "Interactable" layer.
-        if (other.gameObject.layer == LayerMask.NameToLayer("Interactable"))
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Interactable"))
         {
             // Set the current interactable object in the interaction controller.
             interactionController.SetCurrentInteractable(other.gameObject);
@@ -29,7 +45,12 @@ public class PlayerCollisionHandler : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Interactable"))
+        // Reset slowdown when leaving the trap.
+        if (other.CompareTag("SlowdownTrap"))
+        {
+            movementController.SetSlowDown(false);
+        }
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Interactable"))
         {
             // Only clear if the object leaving is the current interactable.
             if (interactionController.GetCurrentInteractable() == other.gameObject)
